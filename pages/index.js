f09e181a9cec1d37447943088f281ce27cdbcb9b
Shahab Laghaee <shahab.itech@gmail.com>
2024-11-15 19:39:15 +0330

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Shirreza!</a>
+          Welcome to <a href="https://nextjs.org">Nextjs!</a>
         </h1>
 
         <p className={styles.description}>
